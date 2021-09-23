ea74a02767968558810a68ed27a7b513753290bd
Nam Truong <sunnam9897@gmail.com>
2021-09-24 00:50:43 +0700

    Updated

--- lib/app/modules/home/views/home_view,desktop.dart
+++ lib/app/modules/home/views/home_view,desktop.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:south_forest_restaurant/app/common/app_constants.dart';
 
 class HomeViewDesktop extends StatelessWidget {
   const HomeViewDesktop({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text(
+        'Hello my pages',
+        style: TextStyle(color: primaryColor),
+      ),
+    );
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:south_forest_restaurant/app/common/app_constants.dart';
 import 'package:south_forest_restaurant/app/modules/home/views/home_view.phone.dart';
 import 'package:south_forest_restaurant/app/modules/home/views/home_view.tablet.dart';
 
@@ -10,7 +12,13 @@ class HomeView extends GetResponsiveView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _buildBody(),
+      backgroundColor: secondaryColor,
+      body: Center(
+        child: Text(
+          'Hello my pages',
+          style: GoogleFonts.roboto(fontSize: 20, color: primaryColor),
+        ),
+      ),
     );
   }
 
